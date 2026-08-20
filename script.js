29a4d44f19088c915c2d06625ca03898ec90e595
KEMMOUNE Houda <113966070+HOUDA-KEMMOUNE@users.noreply.github.com>
2026-08-20 23:11:49 +0000

    Counting people page => Done

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
-// let count = document.getElementById("_count");
-// count.innerText = 3;
+let		count = 0;
+
+function	increment()
+{
+	count++;
+	console.log(count);
+	document.getElementById("_count").innerText = count;
+}
+
+function	decrement()
+{
+	if ( count === 0 )
+		return ;
+	count--;
+	console.log(count);
+	document.getElementById("_count").innerText = count;
+}
